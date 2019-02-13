025b564db43a8f5c4cc907c9325ede376d3b0445
Arif Roktim <aroktim@stuy.edu>
2019-02-13 13:56:28 -0500

    Replace Point from being struct tuple to a struct

--- src/main.rs
+++ src/main.rs
@@ -103,13 +103,16 @@ impl fmt::Display for Color {
 
 // Point(x, y)
 #[derive(Debug)]
-struct Point(usize, usize);
+struct Point {
+    x: usize,
+    y: usize,
+}
 
 impl Point {
     // If undefined, return None, otherwise return Some(f64)
     fn slope(&self, other: &Point) -> Option<f64> {
-        let me = (self.0 as f64, self.1 as f64);
-        let other = (other.0 as f64, other.1 as f64);
+        let me = (self.x as f64, self.y as f64);
+        let other = (other.x as f64, other.y as f64);
         if me.0 - other.0 == 0.0 {
             None
         } else {
@@ -120,7 +123,7 @@ impl Point {
 
 impl fmt::Display for Point {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}, {}", self.0, self.1)
+        write!(f, "{}, {}", self.x, self.y)
     }
 }
 
@@ -169,21 +172,24 @@ impl Screen {
     fn draw_point(&mut self, p: &Point, c: Color) -> Result<(), OutOfBounds> {
         // Make (0, 0) the bottom left corner instead of
         // the top left corner
-        if p.0 >= COLUMNS || p.1 >= ROWS {
-            let point = Point(p.0, p.1);
+        if p.x >= COLUMNS || p.y >= ROWS {
+            let point = Point { x: p.x, y: p.y };
             return Err(OutOfBounds(point));
         }
-        let p = Point(p.0, ROWS - 1 - p.1);
+        let p = Point {
+            x: p.x,
+            y: ROWS - 1 - p.y,
+        };
         // Get the pixel ay point p and set its color
         // Man this looks ugly :(
-        &self.pixels[p.1][p.0].color(c);
+        &self.pixels[p.y][p.x].color(c);
         Ok(())
     }
 
     fn draw_line(&mut self, p0: &Point, p1: &Point, c: Color) -> Result<(), OutOfBounds> {
         // this draws from left to right, from p0 to p1
         // if p0 is to the right of p1, swap them
-        if p0.0 > p1.0 {
+        if p0.x > p1.x {
             self.draw_line(p1, p0, c)?;
             return Ok(());
         }
@@ -192,7 +198,7 @@ impl Screen {
             // self._vertical_line(p0, p1, ...) assumes that
             // p0's y value is less than that of p1.
             // This accounts for that case.
-            None if p0.1 <= p1.1 => self._vertical_line(p0, p1, c)?,
+            None if p0.y <= p1.y => self._vertical_line(p0, p1, c)?,
             // This accounts for case where p1's y value > p0's y value
             None => self._vertical_line(p1, p0, c)?,
             Some(m) if m == 0.0 => self._horizontal_line(p0, p1, c)?,
@@ -207,21 +213,21 @@ impl Screen {
 
     // ========== HELPER FUNCTIONS START ==========
     fn _vertical_line(&mut self, p0: &Point, p1: &Point, c: Color) -> Result<(), OutOfBounds> {
-        for i in p0.1..p1.1 {
-            self.draw_point(&Point(p0.0, i), c)?;
+        for i in p0.y..p1.y {
+            self.draw_point(&Point { x: p0.x, y: i }, c)?;
         }
         Ok(())
     }
     fn _horizontal_line(&mut self, p0: &Point, p1: &Point, c: Color) -> Result<(), OutOfBounds> {
-        for i in p0.0..p1.0 {
-            self.draw_point(&Point(i, p0.1), c)?;
+        for i in p0.x..p1.x {
+            self.draw_point(&Point { x: i, y: p0.y }, c)?;
         }
         Ok(())
     }
     fn _octant1(&mut self, p0: &Point, p1: &Point, c: Color) -> Result<(), OutOfBounds> {
         // First cast the points to i32 from usize
-        let p0 = (p0.0 as i32, p0.1 as i32);
-        let p1 = (p1.0 as i32, p1.1 as i32);
+        let p0 = (p0.x as i32, p0.y as i32);
+        let p1 = (p1.x as i32, p1.y as i32);
         // x and y points to plot
         let mut x = p0.0;
         let mut y = p0.1;
@@ -231,14 +237,20 @@ impl Screen {
         let delta_y = p1.1 - p0.1;
         // d = f(x0 + 1, y0 + 1/2) - f(x0, y0)
         // f(x0, y0) = 0
-        // d = f(x0 + 1, y0 + 1/2) 
+        // d = f(x0 + 1, y0 + 1/2)
         // ... <Algebra goes here> ...
         // d = delta_y - 1/2 * delta_x
         // To get rid of floating point arithmetic, multiply by 2
         // 2d = 2 * delta_y - delta_x
         let mut diff = 2 * delta_y - delta_x;
         while x <= p1.0 {
-            self.draw_point(&Point(x as usize, y as usize), c)?;
+            self.draw_point(
+                &Point {
+                    x: x as usize,
+                    y: y as usize,
+                },
+                c,
+            )?;
             if diff > 0 {
                 y += 1;
                 diff -= 2 * delta_x;
@@ -250,8 +262,8 @@ impl Screen {
     }
     fn _octant2(&mut self, p0: &Point, p1: &Point, c: Color) -> Result<(), OutOfBounds> {
         // First cast the points to i32 from usize
-        let p0 = (p0.0 as i32, p0.1 as i32);
-        let p1 = (p1.0 as i32, p1.1 as i32);
+        let p0 = (p0.x as i32, p0.y as i32);
+        let p1 = (p1.x as i32, p1.y as i32);
         // x and y points to plot
         let mut x = p0.0;
         let mut y = p0.1;
@@ -265,7 +277,13 @@ impl Screen {
         // 2d = delta_y - 2 * delta_x
         let mut diff = delta_y - 2 * delta_x;
         while y <= p1.1 {
-            self.draw_point(&Point(x as usize, y as usize), c)?;
+            self.draw_point(
+                &Point {
+                    x: x as usize,
+                    y: y as usize,
+                },
+                c,
+            )?;
             if diff < 0 {
                 x += 1;
                 diff += 2 * delta_y;
@@ -277,8 +295,8 @@ impl Screen {
     }
     fn _octant8(&mut self, p0: &Point, p1: &Point, c: Color) -> Result<(), OutOfBounds> {
         // First cast the points to i32 from usize
-        let p0 = (p0.0 as i32, p0.1 as i32);
-        let p1 = (p1.0 as i32, p1.1 as i32);
+        let p0 = (p0.x as i32, p0.y as i32);
+        let p1 = (p1.x as i32, p1.y as i32);
         // x and y points to plot
         let mut x = p0.0;
         let mut y = p0.1;
@@ -294,7 +312,13 @@ impl Screen {
         // 2d = 2 * delta_y + delta_x
         let mut diff = 2 * delta_y + delta_x;
         while x <= p1.0 {
-            self.draw_point(&Point(x as usize, y as usize), c)?;
+            self.draw_point(
+                &Point {
+                    x: x as usize,
+                    y: y as usize,
+                },
+                c,
+            )?;
             if diff > 0 {
                 y -= 1;
                 diff -= 2 * delta_x;
@@ -306,8 +330,8 @@ impl Screen {
     }
     fn _octant7(&mut self, p0: &Point, p1: &Point, c: Color) -> Result<(), OutOfBounds> {
         // First cast the points to i32 from usize
-        let p0 = (p0.0 as i32, p0.1 as i32);
-        let p1 = (p1.0 as i32, p1.1 as i32);
+        let p0 = (p0.x as i32, p0.y as i32);
+        let p1 = (p1.x as i32, p1.y as i32);
         // x and y points to plot
         let mut x = p0.0;
         let mut y = p0.1;
@@ -324,7 +348,13 @@ impl Screen {
         let mut diff = delta_y + 2 * delta_x;
         //let mut diff = -2 * delta_x - delta_y;
         while y >= p1.1 {
-            self.draw_point(&Point(x as usize, y as usize), c)?;
+            self.draw_point(
+                &Point {
+                    x: x as usize,
+                    y: y as usize,
+                },
+                c,
+            )?;
             if diff < 0 {
                 x += 1;
                 diff -= 2 * delta_y;
@@ -373,65 +403,99 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut screen = Screen::blank();
     //let mut screen = Screen::new(Color::white());
 
-    screen.draw_point(&Point(25, 25), Color::white())?;
-    screen.draw_point(&Point(0, 0), Color::white())?;
+    screen.draw_point(&Point { x: 25, y: 25 }, Color::white())?;
+    screen.draw_point(&Point { x: 0, y: 0 }, Color::white())?;
     // Out of bounds
     // Error: OutOfBounds(Point(100, 2000))
     //screen.draw_point(&Point(100, 2000), Color::white())?;
-    let somepoint = Point(250, 250);
+    let somepoint = Point { x: 250, y: 250 };
 
     // === Test different line types ===
     // vertical line
-    screen.draw_line(&somepoint, &Point(250, 400), Color::cyan())?;
-    screen.draw_line(&Point(150, 500), &Point(150, 400), Color::cyan())?;
+    screen.draw_line(&somepoint, &Point { x: 250, y: 400 }, Color::cyan())?;
+    screen.draw_line(
+        &Point { x: 150, y: 500 },
+        &Point { x: 150, y: 400 },
+        Color::cyan(),
+    )?;
     // horizontal line
-    screen.draw_line(&somepoint, &Point(400, 250), Color::purple())?;
-    screen.draw_line(&Point(400, 150), &Point(250, 150), Color::purple())?;
+    screen.draw_line(&somepoint, &Point { x: 400, y: 250 }, Color::purple())?;
+    screen.draw_line(
+        &Point { x: 400, y: 150 },
+        &Point { x: 250, y: 150 },
+        Color::purple(),
+    )?;
     // octant 1
-    screen.draw_line(&Point(300, 300), &Point(400, 350), Color::green())?;
-    screen.draw_line(&somepoint, &Point(500, 500), Color::green())?;
+    screen.draw_line(
+        &Point { x: 300, y: 300 },
+        &Point { x: 400, y: 350 },
+        Color::green(),
+    )?;
+    screen.draw_line(&somepoint, &Point { x: 500, y: 500 }, Color::green())?;
     // octant 2
-    screen.draw_line(&Point(300, 300), &Point(350, 400), Color::yellow())?;
-    screen.draw_line(&Point(700, 650), &Point(500, 400), Color::yellow())?;
+    screen.draw_line(
+        &Point { x: 300, y: 300 },
+        &Point { x: 350, y: 400 },
+        Color::yellow(),
+    )?;
+    screen.draw_line(
+        &Point { x: 700, y: 650 },
+        &Point { x: 500, y: 400 },
+        Color::yellow(),
+    )?;
     // octant 8
-    screen.draw_line(&Point(350, 400), &Point(400, 350), Color::red())?;
-    screen.draw_line(&Point(350, 400), &Point(500, 375), Color::red())?;
+    screen.draw_line(
+        &Point { x: 350, y: 400 },
+        &Point { x: 400, y: 350 },
+        Color::red(),
+    )?;
+    screen.draw_line(
+        &Point { x: 350, y: 400 },
+        &Point { x: 500, y: 375 },
+        Color::red(),
+    )?;
     // octant 7
-    screen.draw_line(&somepoint, &Point(300, 150), Color::white())?;
-    screen.draw_line(&Point(275, 100), &somepoint, Color::white())?;
+    screen.draw_line(&somepoint, &Point { x: 300, y: 150 }, Color::white())?;
+    screen.draw_line(&Point { x: 275, y: 100 }, &somepoint, Color::white())?;
 
     // do a frick ton of tests by drawing a circle
-    let center = Point(550, 200);
+    let center = Point { x: 550, y: 200 };
     let radius: f64 = 150.0;
-    for x in (center.0 - radius as usize)..(center.0 + radius as usize + 1) {
+    for x in (center.x - radius as usize)..(center.x + radius as usize + 1) {
         // (x-h)^2 + (y-k)^2 = r^2
         // ... algebra here ...
         // y = k +- sqrt(r^2 - (x-h)^2 )
         // This is a crap way to draw a circle. Since I only increment x by 1,
         //   the circle has very few lines at 0 and pi radians.
         // But it's good enough for showing the different octants
-        let inside = radius.powf(2.0)  - (x as f64 - center.0 as f64).powf(2.0);
+        let inside = radius.powf(2.0) - (x as f64 - center.x as f64).powf(2.0);
 
-        let other = Point(x, (center.1 as f64 + inside.sqrt()) as usize);
+        let other = Point {
+            x,
+            y: (center.y as f64 + inside.sqrt()) as usize,
+        };
         screen.draw_line(&center, &other, color_the_slope(&center, &other))?;
         // self.draw_line(p0, p1) is the same as self.draw_line(p1, p0)
         //screen.draw_line(&other, &center, color_the_slope(&center, &other))?;
 
-        let other = Point(x, (center.1 as f64 - inside.sqrt()) as usize);
+        let other = Point {
+            x,
+            y: (center.y as f64 - inside.sqrt()) as usize,
+        };
         screen.draw_line(&center, &other, color_the_slope(&center, &other))?;
 
-        //let y = (center.1 as f64 - inside.sqrt()) as usize;
+        //let y = (center.y as f64 - inside.sqrt()) as usize;
         //screen.draw_line(&center, &Point(x, y), Color::green())?;
     }
 
     screen.write("out.ppm").expect("Failed to write to file!");
 
     let info = "Vertical lines are cyan.\n\
-        Horizontal lines are purple.\n\
-        Octant 1 lines are green.\n\
-        Octant 2 lines are yellow.\n\
-        Octant 8 lines are red.\n\
-        Octant 7 lines are white.";
+                Horizontal lines are purple.\n\
+                Octant 1 lines are green.\n\
+                Octant 2 lines are yellow.\n\
+                Octant 8 lines are red.\n\
+                Octant 7 lines are white.";
     println!("{}", info);
 
     Ok(())
