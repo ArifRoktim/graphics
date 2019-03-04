800ffc42d70ffadc7e29f34797a1ca035150570c
Arif Roktim <aroktim@stuy.edu>
2019-03-03 21:00:22 -0500

    Start w03_transform

--- README.md
+++ README.md
@@ -1,13 +1,28 @@
-# w02\_matrix
+# w03\_transformation
 
 Assignment:
 
-* General Matrix stuff
-  * matrix multiplication
-  * creation of an identity matrix
-  * displaying a matrix in a reasonable manner
-* Graphics matrix stuff
-  * add a point to an edge matrix
-  * add an edge to an edge matrix (should call your add point routine)
-  * go through an edge matrix and draw the lines stored in the matrix (should call your draw line routine)
-* Create a main function/method that demonstrates all the of matrix routines you wrote, and generates an image using the edge matrix structure. Upload the image to the gallery page.
+1. Write code to work with transformation matrices:
+    * create a translation matrix
+    * create a scale matrix
+    * create a rotation matrix about the x-axis
+    * create a rotation matrix about the y-axis
+    * create a rotation matrix about the z-axis
+      * Note: The trig functions in python and c take radians as parameters, but you should assume degree input, make sure to convert or things won't look right.
+2. Modify your main routine so that it keeps track of:
+    * A single edge matrix
+    * A single master transformation matrix
+3. Create a parser that will interpret a script to be used to draw an image.
+    * Each command is a single word without spaces in it, and if it takes arguments, the line after will contain the arguments, separated by spaces. For example, a line of the file might look like this:
+      * line
+      * 0 0 0 100 100 0
+    * Here is the full list of commands:
+      * line: add a line to the point matrix -  takes 6 arguemnts (x0, y0, z0, x1, y1, z1)
+      * ident: set the transform matrix to the identity matrix
+      * scale: create a scale matrix, then multiply the transform matrix by the scale matrix -  takes 3 arguments (sx, sy, sz)
+      * move: create a translation matrix, then multiply the transform matrix by the translation matrix - takes 3 arguments (tx, ty, tz)
+      * rotate: create a rotation matrix, then multiply the transform matrix by the rotation matrix - takes 2 arguments (axis theta)
+      * apply: apply the current transformation matrix to the edge matrix
+      * display: clear the screen, draw the lines of the point matrix to the screen, display the screen
+      * save: clear the screen, draw the lines of the point matrix to the screen/frame save the screen/frame to a file - takes 1 argument (file name)\
+4. The included script tests the various transformations, it should be used when I run make. You still need to create your own image and submit it to the gallery. Please include your script code only when you upload code to the gallery, this way it will be easy for others to try out your pictures!
--- src/main.rs
+++ src/main.rs
@@ -1,149 +1,16 @@
 use std::error::Error;
 
-mod screen;
-mod matrix;
+//mod screen;
+//mod matrix;
 
-use screen::Screen;
-use screen::Color;
-use matrix::Matrix;
+//use screen::Screen;
+//use screen::Color;
+//use matrix::Matrix;
 
 const COLUMNS: usize = 500;
 const ROWS: usize = 500;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let m0 = Matrix::new(1);
-    println!("\nTesting new(). m0=\n{}", m0);
-    let mut m2 = Matrix::new(0);
-    m2.add_point(1., 2., 3.);
-    m2.add_point(4., 5., 6.);
-    println!("\nTesting add_point(). m2=\n{}", m2);
-    let mut m1 = Matrix::new(4);
-    m1.ident();
-    println!("\nTesting ident(). m1=\n{}", m1);
-    m1.mult(&mut m2);
-    println!("\nTesting mult(). m1 * m2=\n{}", m2);
-    let mut m1 = Matrix::new(0);
-    //m1.add_point(1., 2., 3.);
-    //m1.add_point(4., 5., 6.);
-    //m1.add_point(7., 8., 9.);
-    //m1.add_point(10., 11., 12.);
-    m1.add_edge(1., 2., 3., 4., 5., 6.);
-    m1.add_edge(7., 8., 9., 10., 11., 12.);
-    println!("\nTesting add_edge(). m1=\n{}", m1);
-    m1.mult(&mut m2);
-    println!("\nTesting mult(). m1 * m2 =\n{}", m2);
-    
-    // Make an image
-    let mut screen = Screen::new(Color::new(234, 100, 89));
 
-    //let mut m = Matrix::new(0);
-    //m.add_edge(0., 0., 0., 255., 255., 255.);
-    //screen.draw_lines(m, Color::black())?;
-
-    //let mut m = Matrix::new(0);
-    //add_semi_circle(&mut m, 250, 250, 100, true);
-    //fill_circle(&mut m, 100, 100, 50);
-    //screen.draw_lines(m, Color::black())?;
-
-    let mut m = Matrix::new(0);
-    add_semi_circle(&mut m, 250, 250, 100, true);
-    screen.draw_lines(m, Color::black())?;
-
-    let mut eyes = Matrix::new(0);
-    fill_circle(&mut eyes, 320, 250, 27);
-    screen.draw_lines(eyes, Color::new(184, 247, 250))?;
-
-    let mut eyes = Matrix::new(0);
-    fill_circle(&mut eyes, 260, 250, 27);
-    screen.draw_lines(eyes, Color::new(184, 247, 250))?;
-
-    let mut fingers = Matrix::new(0);
-    add_semi_circle(&mut fingers, 340, 150, 10, false);
-    screen.draw_lines(fingers, Color::black())?;
-
-    let mut fingers = Matrix::new(0);
-    add_semi_circle(&mut fingers, 320, 150, 10, false);
-    screen.draw_lines(fingers, Color::black())?;
-
-    let mut fingers = Matrix::new(0);
-    add_semi_circle(&mut fingers, 300, 150, 10, false);
-    screen.draw_lines(fingers, Color::black())?;
-
-    let mut fingers = Matrix::new(0);
-    add_semi_circle(&mut fingers, 280, 150, 10, false);
-    screen.draw_lines(fingers, Color::black())?;
-
-    // ====
-    let mut fingers = Matrix::new(0);
-    add_semi_circle(&mut fingers, 340, 151, 10, false);
-    screen.draw_lines(fingers, Color::black())?;
-
-    let mut fingers = Matrix::new(0);
-    add_semi_circle(&mut fingers, 320, 151, 10, false);
-    screen.draw_lines(fingers, Color::black())?;
-
-    let mut fingers = Matrix::new(0);
-    add_semi_circle(&mut fingers, 300, 151, 10, false);
-    screen.draw_lines(fingers, Color::black())?;
-
-    let mut fingers = Matrix::new(0);
-    add_semi_circle(&mut fingers, 280, 151, 10, false);
-    screen.draw_lines(fingers, Color::black())?;
-
-    let mut m = Matrix::new(0);
-    m.add_edge(150., 250., 0., 150., 0., 0.);
-    m.add_edge(350., 223., 0., 350., 150., 0.);
-    m.add_edge(330., 200., 0., 330., 150., 0.);
-    m.add_edge(310., 200., 0., 310., 150., 0.);
-    m.add_edge(290., 200., 0., 290., 150., 0.);
-    m.add_edge(270., 200., 0., 270., 150., 0.);
-    m.add_edge(320., 140., 0., 320., 0., 0.);
-    screen.draw_lines(m, Color::black())?;
-
-
-    screen.write("out.ppm")?;
     Ok(())
-}
-
-fn add_semi_circle(m: &mut Matrix, h: usize, k: usize, r: usize, top: bool) {
-    let start: usize;
-    let end: usize;
-    if top {
-        start = k;
-        end = k + r;
-    } else {
-        start = k - r;
-        end = k;
-    }
-    for x in (h-r)..(h+r) { for y in start..end{
-        let sqrt: f64 = (r as f64).powf(2.) - (x as f64 - h as f64).powf(2.);
-        let val: f64;
-        if top {
-            val = k as f64 + sqrt.sqrt();
-        } else {
-            val = k as f64 - sqrt.sqrt();
-        }
-        if y == val as usize {
-            m.add_point(x as f64, val, 0.);
-        }
-    }}
-}
-
-fn add_circle(m: &mut Matrix, h: usize, k: usize, r: usize) {
-    add_semi_circle(m, h, k, r, true);
-    add_semi_circle(m, h, k, r, false);
-}
-
-fn fill_circle(m: &mut Matrix, h: usize, k: usize, r: usize) {
-    for x in (h-r)..(h+r) { for y in (k-r)..(k+r){
-        let sqrt: f64 = (r as f64).powf(2.) - (x as f64 - h as f64).powf(2.);
-        let pos = k as f64 + sqrt.sqrt();
-        let neg = k as f64 - sqrt.sqrt();
-        if y == pos as usize {
-            m.add_point(x as f64, pos, 0.);
-        }
-        if y == neg as usize {
-            m.add_point(x as f64, neg, 0.);
-        }
-    }}
 }
